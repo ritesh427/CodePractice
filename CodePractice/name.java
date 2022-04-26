1ca2b024b7a916c1cc810c18a368d65494fccfe7
Ritesh <riteshpandey427@gmail.com>
2022-04-27 04:38:34 +0530

    Reset

--- CodePractice/name.java
+++
@@ -1,7 +0,0 @@
-public class name {
-    public static void main(String[] args) {
-        String name = System.console().readLine();
-        System.out.println("My name is "+ name);
-    }
-    
-}
